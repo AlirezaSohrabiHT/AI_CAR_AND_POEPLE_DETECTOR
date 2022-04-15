1655b25fb8a3573b0e362f940a432d2e72f9665d
Alireza <alireza@alireza.localdomin>
2022-04-15 13:28:36 +0430

    Detect Cars Coordinates

--- DETECTOR.py
+++ DETECTOR.py
@@ -4,5 +4,7 @@ classifier_file = 'cars.xml'
 img = cv2.imread(img_file)
 black_n_white = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
 car_tracker = cv2.CascadeClassifier(classifier_file)
+cars = car_tracker.detectMultiScale(black_n_white)
+print (cars)
 cv2.imshow('DETECTOR',black_n_white)
 cv2.waitKey()
