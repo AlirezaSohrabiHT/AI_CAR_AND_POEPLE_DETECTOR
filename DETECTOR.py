4e3ba6595682cbf80bf92bfffbb24a86469ab770
Alireza <alireza@alireza.localdomin>
2022-04-15 15:46:49 +0430

    Add Video File And Play On Screen And Print Coordinates Of Cars

--- DETECTOR.py
+++ DETECTOR.py
@@ -1,12 +1,22 @@
 import cv2 
 img_file = 'cars.jpg'
+video = cv2.VideoCapture('cars.mp4')
 classifier_file = 'cars.xml'
-img = cv2.imread(img_file)
-black_n_white = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
 car_tracker = cv2.CascadeClassifier(classifier_file)
-cars = car_tracker.detectMultiScale(black_n_white)
-for(x,y,w,h) in cars:
- cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+while True:
+ (read_successful, frame)=video.read() 
+ if read_successful:
+      grayscaled_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+ else: 
+      break
+
+ cars = car_tracker.detectMultiScale(grayscaled_frame)
+ print(cars)
+ cv2.imshow('DETECTOR',grayscaled_frame)
+ cv2.waitKey(1)
+
+#img = cv2.imread(img_file)
+#black_n_white = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
+#for(x,y,w,h) in cars:
+#cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
 #print (cars)
-cv2.imshow('DETECTOR',img)
-cv2.waitKey()
