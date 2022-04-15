9678b6fb493585d903fd8b9fd5bd9901e069d123
Alireza <alireza@alireza.localdomin>
2022-04-15 12:53:38 +0430

    Turn Img To Black And White And Show On Screen

--- DETECTOR.py
+++ DETECTOR.py
@@ -1,3 +1,8 @@
 import cv2 
 img_file = 'cars.jpg'
 classifier_file = 'cars.xml'
+img = cv2.imread(img_file)
+black_n_white = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
+car_tracker = cv2.CascadeClassifier(classifier_file)
+cv2.imshow('DETECTOR',black_n_white)
+cv2.waitKey()
